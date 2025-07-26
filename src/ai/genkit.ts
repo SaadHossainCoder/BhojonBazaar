fb1e76d86497ee24a3863248755eb777e3255ebd
The Kingdom <thekingdom.saad.69@gmail.com>
2025-07-26 09:56:57 +0000

    File changes

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,16 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import {defineDotenv} from 'genkit';
+
+defineDotenv({
+  experimentalAllowUnsafeGet: true,
+});
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: process.env.GEMINI_API_KEY,
+    }),
+  ],
   model: 'googleai/gemini-2.0-flash',
 });
---
+++ src/lib/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "street-eats-vendor-dda0c",
+  "appId": "1:143094775808:web:e471a52ddb31b6beb258e5",
+  "storageBucket": "street-eats-vendor-dda0c.firebasestorage.app",
+  "apiKey": "AIzaSyBQkNGMzHkpBQSlHcH97A1uJ7iaQsmNrZA",
+  "authDomain": "street-eats-vendor-dda0c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "143094775808"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
